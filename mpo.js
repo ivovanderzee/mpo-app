da63f99287d8c117f3334e4cedb3c2c425ae732e
ivovanderzee <ivovanderzee@live.nl>
2020-11-25 15:38:36 +0100

    Function draggable
    Added a function so users can drag the popover element to another place on the page

--- mpo.js
+++ mpo.js
@@ -80,7 +80,7 @@ popover.style.position = 'fixed';
 popover.style.cursor = 'move';
 popover.style.top = '50px';
 popover.style.left = '550px';
-popover.className = 'popupContent';
+popover.className = 'pop-over';
 popover.style.boxShadow = '8px 5px 5px -3px rgba(0,0,0,0.1), 5px 8px 5px -3px rgba(0,0,0,0.1)';
 
 //Append the new popover to the body
@@ -278,3 +278,45 @@ style.innerHTML = `
 `
 head.appendChild(style);
 
+
+
+// Make the DIV element draggable:
+dragElement(document.querySelector(".pop-over"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV: 
+    elmnt.onmousedown = dragMouseDown;
+  }
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
